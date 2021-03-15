f68523be42c2cfb4e6f5e59065c5aeeb17a5e27e
Harshdeep Singh <hdsingharora99@gmail.com>
2021-03-16 01:39:05 +0530

    removed /build from gitignore

---
+++ client/build/precache-manifest.44957eb9404e184d997cc11b29835962.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "632c0cff03199e0becf3",
+    "url": "/static/css/main.11b30dd0.chunk.css"
+  },
+  {
+    "revision": "632c0cff03199e0becf3",
+    "url": "/static/js/main.632c0cff.chunk.js"
+  },
+  {
+    "revision": "481d67ce02d7d2eb93de",
+    "url": "/static/js/1.481d67ce.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40b4f9a3aa77b20ebc157005bc3b70f2",
+    "url": "/index.html"
+  }
+];
